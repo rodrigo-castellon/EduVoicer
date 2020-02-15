0ede2d9a482d5d02dc79534fbae89b97b9ae20c8
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2020-02-14 22:45:14 -0800

    set up basic stuff

---
+++ pdf_utils.py
@@ -0,0 +1,19 @@
+'''
+Has methods relating to processing PDFs/images
+'''
+
+# import google cloud here
+
+def pdf2text(fname):
+    '''
+    Turn a PDF file `fname` into text
+    '''
+    # first take certain pages of it
+
+    # then turn each page into text
+
+    # then fuse all of it together and return
+
+
+if __name__ == '__main__':
+    pass
